fac2d226c36f9df5bf0c095be7888f6bb736f59c
DyanGalih <dyan.galih@gmail.com>
2023-12-22 01:17:08 +0700

    Initial commit

--- scan.sh
+++ scan.sh
@@ -1,22 +1,3 @@
-# First Aid Guide for Manual and Free Slot Gacor(Panduan Pertolongan Pertama Pada Slot Gacor Secara Manual Dan Gratis)
-
-This bash script reads a list of search strings from a file, searches for these strings in a specified directory and its subdirectories, and prints the unique results.
-
-## Usage
-
-To use this script, you need to call it with the path to the directory you want to search as an argument. For example:
-
-```bash
-sh scan.sh /var/www/html
-```
-
-In this example, the script will search for the strings in the `/var/www/html` directory and its subdirectories.
-
-## Script
-
-Here is the script:
-
-```bash
 #!/bin/bash
 
 # Check if an argument was provided
@@ -47,10 +28,3 @@ readarray -t unique_grep_results < <(printf '%s\n' "${grep_results[@]}" | sort -
 
 # Display the unique results
 printf '%s\n' "${unique_grep_results[@]}"
-```
-
-## Input File
-
-The script reads the search strings from a file named `list.txt`. Each search string should be on its own line in this file.
-
----
